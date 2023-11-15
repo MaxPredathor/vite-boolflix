82c403b50118a2bac31ac3152f2ba0066179809c
MaxPredathor <max.gilli98@gmail.com>
2023-11-15 12:10:24 +0100

    repo + scaffolding

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    pippo
+  </div>
+</template>
+
+<script>
+import { store } from './assets/data/store.js'
+import axios from 'axios'
+  export default {
+    name: 'App',
+    data(){
+    return{
+      store,
+    }
+  }
+  }
+  
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/assets/data/store.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  API_KEY: "52dfd4e50346e927a3b341e688889302",
+});
